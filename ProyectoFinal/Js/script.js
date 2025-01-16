39e8df18c24a7040ee01b0a7a53dea33bf6802f1
AlejoVera97 <alejoveralaffratta21@gmail.com>
2025-01-15 22:02:01 -0300

    Slider index

--- ProyectoFinal/Js/script.js
+++ ProyectoFinal/Js/script.js
@@ -38,3 +38,27 @@ function moveSlide(n) {
 
 
 
+
+
+
+// section madera
+
+const slider = document.querySelector(".hover-slider-container");
+let isDragging = false;
+let startX, scrollLeft;
+
+// Evento para arrastrar el slider
+slider.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => isDragging = false);
+slider.addEventListener("mouseup", () => isDragging = false);
+slider.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; // Velocidad del desplazamiento
+    slider.scrollLeft = scrollLeft - walk;
+});
